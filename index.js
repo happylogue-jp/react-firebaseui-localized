0a703ba72eb9050aa2248cad3ae765b3f7e0320d
Theertha TK <theertha@centinora.com>
2020-06-02 17:19:19 +0530

    Babel compile to ES5

--- index.js
+++ index.js
@@ -1,41 +1,104 @@
-import React, { useEffect, useRef } from "react";
-import useScript from "./useScript";
+"use strict";
 
-const firebaseui_src = lang =>
-  `https://www.gstatic.com/firebasejs/ui/4.1.0/firebase-ui-auth__${lang}.js`;
-const FIREBASEUI_CONTAINER_ID = "firebaseui_container";
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-function FirebaseUIAuth({ auth, config, lang, firebase }) {
-  const [loaded, error] = useScript(firebaseui_src(lang));
-  const container = useRef();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
 
-  useEffect(() => {
+var _react = _interopRequireWildcard(require("react"));
+
+var _useScript3 = _interopRequireDefault(require("./useScript"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var firebaseui_src = function firebaseui_src(lang) {
+  return "https://www.gstatic.com/firebasejs/ui/4.1.0/firebase-ui-auth__".concat(lang, ".js");
+};
+
+var FIREBASEUI_CONTAINER_ID = "firebaseui_container";
+
+function FirebaseUIAuth(_ref) {
+  var auth = _ref.auth,
+      config = _ref.config,
+      lang = _ref.lang,
+      firebase = _ref.firebase;
+
+  var _useScript = (0, _useScript3.default)(firebaseui_src(lang)),
+      _useScript2 = _slicedToArray(_useScript, 2),
+      loaded = _useScript2[0],
+      error = _useScript2[1];
+
+  var container = (0, _react.useRef)();
+  (0, _react.useEffect)(function () {
     window.firebase = firebase;
   }, []);
-  useEffect(() => {
+  (0, _react.useEffect)(function () {
     if (!loaded) return;
     if (error) throw error;
-    (async () => {
-      const app = window.firebase.apps.find(
-        ({ name }) => name.endsWith('-firebaseui-temp')
-      );
-      if (app) await app.delete();
-      if (container.current) container.current.innerHTML = "";
-      const firebaseUI = new window.firebaseui.auth.AuthUI(auth);
-      firebaseUI.start(`#${FIREBASEUI_CONTAINER_ID}`, config);
-    })();
-  }, [auth, config, error, loaded]);
 
-  return (
-    <>
-      <link
-        type="text/css"
-        rel="stylesheet"
-        href="https://www.gstatic.com/firebasejs/ui/4.1.0/firebase-ui-auth.css"
-      />
-      <div ref={container} id={FIREBASEUI_CONTAINER_ID} />
-    </>
-  );
+    _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+      var app, firebaseUI;
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              app = window.firebase.apps.find(function (_ref3) {
+                var name = _ref3.name;
+                return name.endsWith('-firebaseui-temp');
+              });
+
+              if (!app) {
+                _context.next = 4;
+                break;
+              }
+
+              _context.next = 4;
+              return app.delete();
+
+            case 4:
+              if (container.current) container.current.innerHTML = "";
+              firebaseUI = new window.firebaseui.auth.AuthUI(auth);
+              firebaseUI.start("#".concat(FIREBASEUI_CONTAINER_ID), config);
+
+            case 7:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }))();
+  }, [auth, config, error, loaded]);
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("link", {
+    type: "text/css",
+    rel: "stylesheet",
+    href: "https://www.gstatic.com/firebasejs/ui/4.1.0/firebase-ui-auth.css"
+  }), /*#__PURE__*/_react.default.createElement("div", {
+    ref: container,
+    id: FIREBASEUI_CONTAINER_ID
+  }));
 }
 
-export default FirebaseUIAuth;
+var _default = FirebaseUIAuth;
+exports.default = _default;
